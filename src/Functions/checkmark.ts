7f032c536856fa5335d86c446eb62ebfc7bd3267
Striatp <sebastien.works@outlook.com>
2025-04-15 17:48:07 +0200

    Add documentation comments for checkmark and crossmark emoji functions.

--- src/Functions/checkmark.ts
+++ src/Functions/checkmark.ts
@@ -1,5 +1,9 @@
 import { IForgeFunction } from "@tryforge/forgescript";
 
+/**
+ * Fetch the 'checkmark' emoji from the app's built-in emojis.
+ * @returns - Emoji
+*/
 const Function: IForgeFunction = {
   name: 'checkmark',
   code: `$return[<:checkmark:$findApplicationEmoji[checkmark]>]`
--- src/Functions/crossmark.ts
+++ src/Functions/crossmark.ts
@@ -1,5 +1,9 @@
 import { IForgeFunction } from "@tryforge/forgescript";
 
+/**
+ * Fetch the 'crossmark' emoji from the app's built-in emojis.
+ * @returns - Emoji
+*/
 const Function: IForgeFunction = {
   name: 'crossmark',
   code: `$return[<:crossmark:$findApplicationEmoji[crossmark]>] `
